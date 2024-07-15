dc55e10df7c92b58522650209a64a7c0a2060f32
mandagalladinesh <mandagalladinesh@gmail.com>
2024-07-15 17:22:49 +0530

    first commit

--- e4ddProject/src/pages/Drone/Drones.jsx
+++ e4ddProject/src/pages/Drone/Drones.jsx
@@ -6,25 +6,25 @@ const Drones = () => {
     <div className="drone-container w-full h-full">
       <div className="imageContainer">
         <img src="./drones/dro1.jpg" alt="Drone" className="image" />
-        <h1 className="title">Drones</h1>
+        <h1 className="title">طائرات بدون طيار</h1>
       </div>
       <div className="content1">
         <div className="drone-section  section1">
           <div className="contentWithImage">
-            <h2 className="sectionTitle">Drones Sales and Rental Services</h2>
+            <h2 className="sectionTitle">خدمات بيع وتأجير الطائرات بدون طيار</h2>
             <p className="description">
-              "Embark on an exhilarating journey into the skies with our premier
-              drone sales and rental services, where every flight is an
-              adventure waiting to unfold."
+            "انطلق في رحلة مبهجة إلى السماء مع رئيس الوزراء لدينا
+              خدمات بيع وتأجير الطائرات بدون طيار، حيث تكون كل رحلة بمثابة
+              مغامرة في انتظار أن تتكشف."
             </p>
             <p className="text">
-              Discover the ultimate destination for drone enthusiasts and
-              professionals alike – our comprehensive drone sales and rental
-              services. Delve into our diverse inventory featuring
-              enterprise-grade drones meticulously selected for their quality,
-              performance, and affordability. Whether you're seeking to own a
-              drone outright or require one for a specific project, we cater to
-              your needs with unparalleled flexibility and competitive pricing.
+            اكتشف الوجهة النهائية لعشاق الطائرات بدون طيار و
+              المهنيين على حدٍ سواء - مبيعاتنا الشاملة وتأجير الطائرات بدون طيار
+              خدمات. الخوض في ميزات المخزون المتنوعة لدينا
+              طائرات بدون طيار من فئة المؤسسات تم اختيارها بدقة لجودتها،
+              الأداء والقدرة على تحمل التكاليف. سواء كنت تسعى لامتلاك
+              طائرة بدون طيار بشكل كامل أو تتطلب واحدة لمشروع معين، نحن نلبي احتياجاتها
+              احتياجاتك بمرونة لا مثيل لها وأسعار تنافسية.
             </p>
           </div>
           <div className="imageRight">
@@ -38,24 +38,24 @@ const Drones = () => {
 
         <div className="drone-section section2">
           <div className="contentWithImage">
-            <h2 className="sectionTitle">Aerial Drone Surveying and Mapping</h2>
+            <h2 className="sectionTitle">المسح الجوي بدون طيار ورسم الخرائط</h2>
             <p className="description">
-              "Elevate your business with our advanced aerial drone surveying
-              and mapping services, where innovation meets precision for
-              unparalleled results."
+            "ارفع مستوى عملك من خلال المسح الجوي المتقدم بطائرات بدون طيار
+              وخدمات رسم الخرائط، حيث يلتقي الابتكار بالدقة
+              نتائج لا مثيل لها."
             </p>
             <p className="text">
-              Unlock the power of precision and efficiency with our
-              revolutionary aerial drone surveying and mapping services,
-              meticulously designed to propel your business to unprecedented
-              heights of success. Harnessing the cutting-edge capabilities of
-              drone technology, we redefine the landscape of traditional
-              surveying methods, ensuring unparalleled accuracy and detail in
-              every map we create. Say farewell to the constraints of outdated
-              techniques and embrace a future where time and resources are
-              optimized to their fullest potential, tailored precisely to meet
-              your evolving needs with unmatched precision and
-              cost-effectiveness.
+            أطلق العنان لقوة الدقة والكفاءة مع منتجاتنا
+              خدمات المسح الجوي ورسم الخرائط الثورية بدون طيار،
+              مصممة بدقة لدفع عملك إلى مستوى غير مسبوق
+              مرتفعات النجاح. تسخير القدرات المتطورة لل
+              تكنولوجيا الطائرات بدون طيار، نعيد تعريف المشهد التقليدي
+              أساليب المسح، وضمان دقة لا مثيل لها والتفصيل في
+              كل خريطة نقوم بإنشائها. قل وداعًا للقيود التي عفا عليها الزمن
+              التقنيات واحتضان المستقبل حيث الوقت والموارد
+              تم تحسينها إلى أقصى إمكاناتها، ومصممة خصيصًا لتلبية احتياجاتها
+              احتياجاتك المتطورة بدقة لا مثيل لها
+              الفعالية من حيث التكلفة.
             </p>
           </div>
           <div className="imageRight">
@@ -70,20 +70,20 @@ const Drones = () => {
         <div className="drone-section section3">
           <div className="contentWithImage">
             <h2 className="sectionTitle">
-              Aerial Drone Inspection and Analysis
+            التفتيش والتحليل الجوي بدون طيار
             </h2>
             <p className="description">
-              "Experience the cutting-edge of asset inspection with our aerial
-              drone services, where safety, efficiency, and precision converge
-              to redefine industry standards."
+            "اختبر أحدث تقنيات فحص الأصول باستخدام هوائينا
+              خدمات الطائرات بدون طيار، حيث تتلاقى السلامة والكفاءة والدقة
+              لإعادة تحديد معايير الصناعة."
             </p>
             <p className="text">
-              Experience the future of asset inspection with our aerial drone
-              inspection and analysis services. Our drones are equipped with
-              high-resolution cameras and sensors to provide you with detailed
-              visual and data insights into your assets. From infrastructure and
-              buildings to pipelines and power lines, our drone inspections
-              offer improved efficiency, greater safety, and reduced risks.
+            اكتشف مستقبل فحص الأصول باستخدام طائرتنا بدون طيار
+              خدمات التفتيش والتحليل. طائراتنا بدون طيار مجهزة
+              كاميرات وأجهزة استشعار عالية الدقة لتزويدك بالتفاصيل
+              رؤى مرئية وبيانات حول الأصول الخاصة بك. من البنية التحتية و
+              المباني وخطوط الأنابيب وخطوط الكهرباء، وتفتيش الطائرات بدون طيار لدينا
+              تقديم كفاءة محسنة، ومزيد من السلامة، وتقليل المخاطر.
             </p>
           </div>
           <div className="imageRight">
@@ -97,20 +97,20 @@ const Drones = () => {
 
         <div className="drone-section section4">
           <div className="contentWithImage">
-            <h2 className="sectionTitle">Drone Videography and Photography</h2>
+            <h2 className="sectionTitle">التصوير بالفيديو والتصوير الفوتوغرافي بدون طيار</h2>
             <p className="description">
-              "Elevate your visuals with our drone videography and photography
-              services. From real estate to events, our skilled team delivers
-              captivating shots that set you apart."
+            "ارفع مستوى صورك من خلال التصوير الفوتوغرافي بالفيديو والتصوير الفوتوغرافي بطائرات بدون طيار
+              خدمات. من العقارات إلى الفعاليات، يقوم فريقنا الماهر بالتوصيل
+              لقطات آسرة تميزك."
             </p>
             <p className="text">
-              Elevate your business's visual presentation with our professional
-              drone videography and photography services, tailored to capture
-              stunning aerial visuals that leave a lasting impression. Whether
-              you're showcasing real estate properties, promoting events, or
-              creating captivating marketing content, our expertly piloted
-              drones and skilled photographers ensure each shot is nothing short
-              of breathtaking.
+            ارفع مستوى العرض المرئي لشركتك مع المحترفين لدينا
+              خدمات التصوير بالفيديو والتصوير الفوتوغرافي بدون طيار، مصممة خصيصًا لالتقاط الصور
+              صور جوية مذهلة تترك انطباعًا دائمًا. سواء
+              كنت تعرض العقارات، أو تروج للأحداث، أو
+              إنشاء محتوى تسويقي جذاب، يتم تجريبه بخبرة
+              الطائرات بدون طيار والمصورين المهرة يضمنون أن كل لقطة ليست قصيرة
+              من لالتقاط الأنفاس.
             </p>
           </div>
           <div className="imageRight">
@@ -124,63 +124,63 @@ const Drones = () => {
 
         <div className="drone-section section5">
           <h2 className="sectionTitle">
-            Why Choose Elite for Digital Development LLC for Your Drone Needs?
+          لماذا تختار شركة Elite for Digital Development LLC لتلبية احتياجاتك من الطائرات بدون طيار؟
           </h2>
           <div className="card-container">
             <div className="row">
               <div className="drone-card">
                 <div className="cardContent">
-                  <strong>Quality Assurance:</strong> We believe in offering
-                  only the highest quality drones that are built to last. Each
-                  drone in our inventory undergoes rigorous testing to ensure
-                  optimal performance and reliability.
+                  <strong>تاكيد الجودة:</strong> نحن نؤمن بالعرض
+                  فقط الطائرات بدون طيار عالية الجودة التي تم تصميمها لتدوم طويلاً. كل
+                  تخضع الطائرة بدون طيار الموجودة في مخزوننا لاختبارات صارمة لضمان ذلك
+                  الأداء الأمثل والموثوقية.
                 </div>
               </div>
               <div className="drone-card">
                 <div className="cardContent">
-                  <strong>Competitive Pricing:</strong> We understand the
-                  importance of staying within budget without compromising on
-                  quality. That's why we strive to offer the best prices on our
-                  enterprise drones, making them accessible to businesses of all
-                  sizes.
+                  <strong>أسعار تنافسية:</strong> نحن نفهم
+                  أهمية البقاء في حدود الميزانية دون المساومة عليها
+                  جودة. ولهذا السبب نسعى جاهدين لتقديم أفضل الأسعار لدينا
+                  طائرات بدون طيار للمؤسسات، مما يجعلها في متناول الشركات للجميع
+                  الأحجام.
                 </div>
               </div>
               <div className="drone-card">
                 <div className="cardContent">
-                  <strong>Custom Solutions:</strong> No two businesses are
-                  alike, which is why we offer customized drone solutions
-                  tailored to your specific needs. Whether you require
-                  additional features, accessories, or software integrations,
-                  we're here to accommodate your unique requirements.
+                  <strong>حلول مخصصة:</strong> لا يوجد شركتان
+                  على حد سواء، ولهذا السبب نقدم حلول الطائرات بدون طيار المخصصة
+                  مصممة خصيصا لاحتياجاتك المحددة. سواء كنت تحتاج
+                  ميزات إضافية أو ملحقات أو عمليات تكامل البرامج،
+                  نحن هنا لتلبية متطلباتك الفريدة.
                 </div>
               </div>
             </div>
             <div className="row">
               <div className="drone-card">
                 <div className="cardContent">
-                  <strong>Expert Guidance:</strong> With years of experience in
-                  the drone industry, our team of experts is here to provide you
-                  with knowledgeable guidance every step of the way. From
-                  selecting the right drone model to offering technical support,
-                  we're committed to helping you make informed decisions.
+                  <strong>إرشادات الخبراء:</strong>مع سنوات من الخبرة في
+                  في صناعة الطائرات بدون طيار، فريق الخبراء لدينا موجود هنا لتزويدك
+                  مع التوجيه المعرفة في كل خطوة على الطريق. من
+                  اختيار طراز الطائرة بدون طيار المناسب لتقديم الدعم الفني،
+                  نحن ملتزمون بمساعدتك على اتخاذ قرارات مستنيرة.
                 </div>
               </div>
               <div className="drone-card">
                 <div className="cardContent">
-                  <strong>Flexible Rental Options:</strong> Not ready to commit
-                  to purchasing a drone outright? No problem. We offer flexible
-                  rental options that allow you to access the latest drone
-                  technology without the upfront investment. Whether you need a
-                  drone for a one-time project or an ongoing venture, we've got
-                  you covered.
+                  <strong>خيارات تأجير مرنة:</strong> غير مستعد للالتزام
+                  لشراء طائرة بدون طيار بشكل مباشر؟ لا مشكلة. نحن نقدم مرنة
+                  خيارات التأجير التي تتيح لك الوصول إلى أحدث طائرة بدون طيار
+                  التكنولوجيا دون الاستثمار مقدما. سواء كنت في حاجة الى
+                  لدينا طائرة بدون طيار لمشروع لمرة واحدة أو مشروع مستمر
+                  قمت بتغطيتها.
                 </div>
               </div>
               <div className="drone-card">
                 <div className="cardContent">
-                  <strong>Customer Satisfaction:</strong> Your satisfaction is
-                  our top priority. We take pride in delivering exceptional
-                  customer service and support to ensure your experience with us
-                  is nothing short of excellent.
+                  <strong>رضا العملاء:</strong> الارتياح الخاص بك هو
+                  أولويتنا القصوى. نحن نفخر بتقديم استثنائي
+                  خدمة العملاء والدعم لضمان تجربتك معنا
+                  ليس أقل من ممتاز.
                 </div>
               </div>
             </div>
